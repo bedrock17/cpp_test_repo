46ac142b127a293dc05ea131645499c81a472e93
SHK <bedrook@focushns.com>
2018-06-29 15:01:34 +0900

    cpp_test_repo

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void) {
+  printf("Hello WOrld\n");
+
+  std::cout << "Hell WOrld \n";
+
+  int N;
+
+  std::cin >> N;
+
+  std::cout << N << std::endl;
+
+  return 0;
+}
